15d38113c782829f0b4c2d42e27e0577318a599c
Chrystine Martins <chrystine.martins@outlook.com>
2026-01-30 14:01:39 -0300

    feat: implementar servidor HTTP e configuração de escuta na porta

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+import { log } from "console";
+import * as http from "http";
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
+
+});
+
+const port = process.env.PORT
+
+server.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado na porta ${port}`);
+})
+
+
