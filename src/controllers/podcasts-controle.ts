9dbf2ed44663719e90c2d2deaa53db56e69fca97
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-02-03 16:13:15 -0300

    feat: add new function to getFilterEpisodes

--- src/controllers/podcasts-controle.ts
+++ src/controllers/podcasts-controle.ts
@@ -1,10 +1,23 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisodes } from "../services/list-episodes-service";
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 
+
+// Lista
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
 
     const content = await serviceListEpisodes();
 
     response.writeHead(200, { 'content-type': "application/json" })
     response.end(JSON.stringify(content));
+}
+
+// Filtro
+export const getFilterEpisodes = async ( request: IncomingMessage, response: ServerResponse) => {
+
+    const content = await serviceFilterEpisodes("flow");
+
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify(content))
+
 }
