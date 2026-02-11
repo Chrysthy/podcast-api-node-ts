db0e018183197e827fddfd23ed6cf402190b5a05
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-10 21:20:31 -0300

    feat: add queryString

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,17 @@ import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-contr
 
 const server = http.createServer(async (request: http.IncomingMessage, response: http.ServerResponse) => {
 
+    //queryString
+    // http://localhost:3333/api/episode?p=flow
+    const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
+
+
     //listar podcasts
-    if (request.method === "GET" && request.url === "/api/list") {
+    if (request.method === "GET" && baseUrl === "/api/list") {
         await getListEpisodes(request, response);
     }
 
-    if(request.method === "GET" && request.url === "/api/episode") {
+    if (request.method === "GET" && baseUrl === "/api/episode") {
         await getFilterEpisodes(request, response);
     }
 
