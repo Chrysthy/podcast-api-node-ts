b248ef399767fa5d8c9499b2d1f91a630b733288
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-02-12 18:14:07 +0000

    fix: spelling correction

--- src/controllers/podcasts-controle.ts
+++ src/controllers/podcasts-controle.ts
@@ -16,7 +16,7 @@ export const getListEpisodes = async (request: IncomingMessage, response: Server
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
 
 
-    const content = await serviceFilterEpisodes(req.url);
+    const content = await serviceFilterEpisodes(request.url);
 
     response.writeHead(200, { "Content-Type": "application/json" });
     response.end(JSON.stringify(content))
