f7d6d78f526c564018b5af2c826f26dae1d231e3
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-15 21:06:22 -0300

    feat: substituir strings literais por enumeração para métodos HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as http from "http";
 import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controle";
 import { Routes } from "./routes/routes";
+import { HttpMethod } from "./utils/http-methods";
 
 const server = http.createServer(async (request: http.IncomingMessage, response: http.ServerResponse) => {
 
@@ -10,11 +11,11 @@ const server = http.createServer(async (request: http.IncomingMessage, response:
 
 
     //listar podcasts
-    if (request.method === "GET" && baseUrl === Routes.LIST) {
+    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
         await getListEpisodes(request, response);
     }
 
-    if (request.method === "GET" && baseUrl === Routes.EPISODE) {
+    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
         await getFilterEpisodes(request, response);
     }
 
