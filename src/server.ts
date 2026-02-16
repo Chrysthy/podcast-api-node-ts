ea751f9afeaf61f94140198741baf31c7560f48d
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-15 22:59:21 -0300

    refactor: simplificar a criação do servidor utilizando criando o app

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,12 @@
 import * as http from "http";
-import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-methods";
+import { app } from "./app";
 
-const server = http.createServer(async (request: http.IncomingMessage, response: http.ServerResponse) => {
+const server = http.createServer(app);
 
-    //queryString
-    // http://localhost:3333/api/episode?p=flow
-    const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
-
-
-    //listar podcasts
-    if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
-        await getListEpisodes(request, response);
-    }
-
-    if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
-        await getFilterEpisodes(request, response);
-    }
-
-});
-
-const port = process.env.PORT
+const port = process.env.PORT;
 
 server.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado na porta ${port}`);
-})
+});
 
 
