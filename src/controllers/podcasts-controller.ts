30884d0578238a116183d9f81126bd64493fd5b0
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-17 15:41:52 -0300

    refactor: corrigir a tipagem do modelo de resposta nos controladores de episódios

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -3,25 +3,27 @@ import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
-import { FilterPodcastModel } from "../models/filter-podcast-model";
+import { PodcastTransferModel } from "../models/filter-podcast-model";
 
 
 // Lista
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
 
-    const content = await serviceListEpisodes();
+    const content: PodcastTransferModel = await serviceListEpisodes();
 
-    response.writeHead(StatusCode.OK, { 'content-type': ContentType.JSON });
-    response.end(JSON.stringify(content));
+    response.writeHead(content.statusCode, { 'content-type': ContentType.JSON });
+    response.write(JSON.stringify(content.body))
+    response.end();
 }
 
 // Filtro
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
 
 
-    const content: FilterPodcastModel = await serviceFilterEpisodes(request.url);
+    const content: PodcastTransferModel = await serviceFilterEpisodes(request.url);
 
     response.writeHead(content.statusCode, { "Content-Type": ContentType.JSON });
-    response.end(JSON.stringify(content.body))
+    response.write(JSON.stringify(content.body))
+    response.end()
 
 }
