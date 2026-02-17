9fa859e102b3fab04262ba3220499e2d585fa5c3
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-16 21:53:00 -0300

    refactor: tipar a resposta do serviço de filtragem de episódios

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -3,6 +3,7 @@ import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 
 
 // Lista
@@ -18,9 +19,9 @@ export const getListEpisodes = async (request: IncomingMessage, response: Server
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
 
 
-    const content = await serviceFilterEpisodes(request.url);
+    const content: FilterPodcastModel = await serviceFilterEpisodes(request.url);
 
-    response.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-    response.end(JSON.stringify(content))
+    response.writeHead(content.statusCode, { "Content-Type": ContentType.JSON });
+    response.end(JSON.stringify(content.body))
 
 }
