664f8a4db4d8a60b5b8fe40955950af44f9fec4c
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-17 15:42:36 -0300

    refactor: tipar o serviço de listagem de episódios para retornar PodcastTransferModel

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,7 +1,23 @@
+import { PodcastTransferModel } from "../models/filter-podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { StatusCode } from "../utils/status-code";
 
-export const serviceListEpisodes = async () => {
+export const serviceListEpisodes = async (): Promise<PodcastTransferModel> => {
+
+    //define contrato
+    let responseFormat: PodcastTransferModel = {
+        statusCode: 0,
+        body: [],
+    }
+
+    //busca os dados
     const data = await repositoryPodcast();
 
-    return data;
+    //verifica o tipo de resposta
+    responseFormat = {
+        statusCode: data.length !== 0 ? StatusCode.OK : StatusCode.NoContent,
+        body: data,
+    }
+
+    return responseFormat;
 }
