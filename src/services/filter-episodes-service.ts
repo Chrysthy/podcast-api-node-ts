d8625c9f073be53bea5ab9c5e5d33dd1522f9cc1
Chrystine Martins <chrystine.martins@outlook.com>
2026-02-16 21:52:50 -0300

    refactor: melhorar a estrutura do serviço de filtragem de episódios

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,10 +1,24 @@
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { StatusCode } from "../utils/status-code";
 
-export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
-    
-     const queryString = podcastName?.split("?p=")[1] || "";
-    
+export const serviceFilterEpisodes = async (podcastName: string | undefined): Promise<FilterPodcastModel> => {
+
+    //define a interface de retorno
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: [],
+    }
+
+    //busca dados
+    const queryString = podcastName?.split("?p=")[1] || "";
+
     const data = await repositoryPodcast(queryString);
-    
-    return data;
+
+    //verifica se tem conteúdo
+    responseFormat.statusCode = data.length !== 0 ? StatusCode.OK : StatusCode.NoContent;
+
+    responseFormat.body = data;
+
+    return responseFormat;
 }
